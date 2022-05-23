db6ffac5cd22413a420cc97eaf06e5053e594b18
ghmaimon <kaixokoko123@gmail.com>
2022-05-23 22:25:59 +0100

    add get current user endpoint

--- src/main/java/com/extensivelyscrum/backend/controller/UserController.java
+++ src/main/java/com/extensivelyscrum/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.extensivelyscrum.backend.controller;
 
 import com.extensivelyscrum.backend.dto.CreateUserDto;
+import com.extensivelyscrum.backend.dto.JwtLoginDto;
 import com.extensivelyscrum.backend.dto.SendMailDto;
 import com.extensivelyscrum.backend.model.User;
 import com.extensivelyscrum.backend.service.EmailService;
@@ -43,6 +44,15 @@ public class UserController {
     public ResponseEntity<Void> sendInvitationEmail(@RequestBody SendMailDto mailDto) {
         emailService.sendInvitationEmail(mailDto.toEmail());
         return new ResponseEntity<>(
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/getCurrent")
+    public ResponseEntity<User> sendInvitationEmail(@RequestHeader("Authorization") String jwtToken) {
+        String email = JwtLoginDto.getEmailFromJwtToken(jwtToken);
+        return new ResponseEntity<>(
+                userService.getUserWithEmail(email),
                 HttpStatus.OK
         );
     }
